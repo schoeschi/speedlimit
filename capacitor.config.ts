85e284345c07d21034742ad229d9bc17f8911479
joshidoshi <joshua.dietiker@gmail.com>
2026-07-29 00:32:23 +0200

    feature: init capacitor app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.thruut.auto",
+  appName: "thruut Auto",
+  webDir: "build",
+};
+
+export default config;
