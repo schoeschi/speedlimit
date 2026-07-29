b4220e423aefed40fc000301e061956404a6d3a2
joshidoshi <joshua.dietiker@gmail.com>
2026-07-30 00:14:33 +0200

    feature: dark mode toggle

--- src/lib/ConfigDialog.svelte
+++ src/lib/ConfigDialog.svelte
@@ -1,9 +1,20 @@
-<script>
+<script lang="ts">
   import { Input } from "$lib/components/ui/input/index.js";
   import { Label } from "$lib/components/ui/label/index.js";
   import * as Dialog from "$lib/components/ui/dialog/index.js";
   import { Button, buttonVariants } from "$lib/components/ui/button/index.js";
   import { Cog } from "@lucide/svelte";
+  import { Switch } from "$lib/components/ui/switch/index.js";
+  import { mode, setMode } from "mode-watcher";
+
+  let checked = $state({
+    get current() {
+      return mode.current === "dark";
+    },
+    set current(checked: boolean) {
+      setMode(checked ? "dark" : "light");
+    }
+  });
 
   let {
 		movingThreshold = $bindable(),
@@ -32,6 +43,11 @@
       <div class="flex flex-col gap-2">
         <Label for="apiEndpoint">API endpoint</Label>
         <Input bind:value={apiEndpoint} id="apiEndpoint" type="url" />
+      </div>
+
+      <div class="flex justify-between">
+        <Label for="darkModeToggle">Dark mode</Label>
+        <Switch id="darkModeToggle" bind:checked={checked.current} />
       </div>
 
       <Dialog.Footer>
